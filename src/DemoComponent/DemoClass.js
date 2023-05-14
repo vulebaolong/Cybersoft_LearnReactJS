4a21e856c7c022f5e68575b6a4ab4bc86536e1e6
Vu Le Bao Long <vulebaolong@gmail.com>
2023-05-14 14:23:54 +0700

    demo

---
+++ src/DemoComponent/DemoClass.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class DemoClass extends Component {
+    render() {
+        return <div className="bg-success">demo class nè</div>;
+    }
+}
