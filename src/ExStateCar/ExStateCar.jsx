63aaa82ea66707c8495386e1d8a52d6607cc0994
Vu Le Bao Long <vulebaolong@gmail.com>
2023-05-14 17:09:02 +0700

    done

--- src/ExStateCar/ExStateCar.jsx
+++ src/ExStateCar/ExStateCar.jsx
@@ -11,46 +11,44 @@ export default class ExStateCar extends Component {
     }
     render() {
         return (
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-8">
-                            <img style={{ width: "100%" }} src={this.state.url} alt="" />
-                        </div>
-                        <div className="col-4 d-flex gap-5">
-                            <button
-                                onClick={() => {
-                                    this.handleColor("black");
-                                }}
-                                className="btn btn-success"
-                            >
-                                Black
-                            </button>
-                            <button
-                                onClick={() => {
-                                    this.handleColor("red");
-                                }}
-                                className="btn btn-success"
-                            >
-                                Red
-                            </button>
-                            <button
-                                onClick={() => {
-                                    this.handleColor("silver");
-                                }}
-                                className="btn btn-success"
-                            >
-                                Silver
-                            </button>
-                            <button
-                                onClick={() => {
-                                    this.handleColor("steel");
-                                }}
-                                className="btn btn-success"
-                            >
-                                Steel
-                            </button>
-                        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-8">
+                        <img style={{ width: "100%" }} src={this.state.url} alt="" />
+                    </div>
+                    <div className="col-4 d-flex gap-5">
+                        <button
+                            onClick={() => {
+                                this.handleColor("black");
+                            }}
+                            className="btn btn-success"
+                        >
+                            Black
+                        </button>
+                        <button
+                            onClick={() => {
+                                this.handleColor("red");
+                            }}
+                            className="btn btn-success"
+                        >
+                            Red
+                        </button>
+                        <button
+                            onClick={() => {
+                                this.handleColor("silver");
+                            }}
+                            className="btn btn-success"
+                        >
+                            Silver
+                        </button>
+                        <button
+                            onClick={() => {
+                                this.handleColor("steel");
+                            }}
+                            className="btn btn-success"
+                        >
+                            Steel
+                        </button>
                     </div>
                 </div>
             </div>
