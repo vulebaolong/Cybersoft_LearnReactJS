127fcbd0433abdd21d86c98f7e17e87449919414
vulebaolong <vulebaolong@gmail.com>
2023-06-09 13:42:01 +0700

    fix  is defined but never used

--- src/Baitap/Lifecycle/Lifecycle.jsx
+++ src/Baitap/Lifecycle/Lifecycle.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Mounting from "./DemoLifecycle/Mounting";
-import Updating from "./DemoLifecycle/Updating";
+// import Mounting from "./DemoLifecycle/Mounting";
+// import Updating from "./DemoLifecycle/Updating";
 import Unmounting from "./DemoLifecycle/Unmounting";
 
 export default class Lifecycle extends Component {
